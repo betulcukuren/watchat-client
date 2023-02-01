db7047736ead3d52039ed1e0feef3780692dee4c
Betul Cukuren <betulcukuren@gmail.com>
2023-02-01 12:19:24 +0300

    update endpoint

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ import VideoPlayer from '../VideoPlayer';
 import Toggles from '../Toggles';
 import './Chat.css';
 
-const ENDPOINT = 'http://localhost:5000';
+const ENDPOINT = 'https://watchatserver.herokuapp.com/';
 const socket = io(ENDPOINT);
 
 const Chat = ({ name }) => {
